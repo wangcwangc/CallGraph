c815f1f37568783a9ab5f9124c47275273cb84e4
wangc <858256500@qq.com>
2021-09-19 21:18:08 +0800

    get all classes stream in jar file

--- src/main/java/callGraph/CallGraph.java
+++ src/main/java/callGraph/CallGraph.java
@@ -1,4 +1,40 @@
 package callGraph;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 public class CallGraph {
+    //将要处理的所有jar的路径
+    List<String> jarPaths;
+    //所有类文件的InputStream
+    List<InputStream> classStream;
+
+    public CallGraph(List<String> jarPaths) {
+        this.jarPaths = jarPaths;
+        try {
+            getAllClassesStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void getAllClassesStream() throws IOException {
+        classStream = new ArrayList<InputStream>();
+        for (String jarPath : jarPaths) {
+            JarFile jarFile = new JarFile(jarPath);
+            Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+            while (jarEntryEnumeration.hasMoreElements()) {
+                JarEntry jarEntry = jarEntryEnumeration.nextElement();
+                if (jarEntry.getName().endsWith(".class")) {
+                    classStream.add(jarFile.getInputStream(jarEntry));
+                }
+            }
+        }
+    }
 }
