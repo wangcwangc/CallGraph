b4b580e3bc8d1877a365cad5341652eed5b253fb
wangc <858256500@qq.com>
2021-09-28 14:14:37 +0800

    add parent and sub class to DCGClassVO

--- src/main/java/callGraph/vo/DCGClassVO.java
+++ src/main/java/callGraph/vo/DCGClassVO.java
@@ -1,5 +1,6 @@
 package callGraph.vo;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class DCGClassVO {
@@ -7,6 +8,8 @@ public class DCGClassVO {
     private String superName;
     private String className;
     private List<DCGMethodVO> methods;
+    private HashSet<DCGClassVO> parentDCGClassVO;//?
+    private HashSet<DCGClassVO> subDCGClassVO;
 
     public DCGClassVO(int access, String superName, String className, List<DCGMethodVO> methods) {
         this.access = access;
@@ -45,5 +48,21 @@ public class DCGClassVO {
 
     public void setMethods(List<DCGMethodVO> methods) {
         this.methods = methods;
+    }
+
+    public void addSubDCGClassVO(DCGClassVO dcgClassVO) {
+        subDCGClassVO.add(dcgClassVO);
+    }
+
+    public HashSet<DCGClassVO> getSubDCGClassVO() {
+        return subDCGClassVO;
+    }
+
+    public void addParentDCGClassVO(DCGClassVO dcgClassVO) {
+        parentDCGClassVO.add(dcgClassVO);
+    }
+
+    public HashSet<DCGClassVO> getParentDCGClassVO() {
+        return parentDCGClassVO;
     }
 }
